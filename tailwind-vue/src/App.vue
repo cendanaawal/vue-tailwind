07889de03cb5e466ee91cedf5e6cbb8e21418a34
cendanaawal <89263129+cendanaawal@users.noreply.github.com>
2022-06-19 09:21:53 +0000

    2

---
+++ tailwind-vue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-full bg-pink-800 h-96 rounded-lg shadow-lg"> 
+
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
